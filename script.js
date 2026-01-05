60441e4f1a7e2dff0b87a40d78a0fbef14c76814
tanvihedau <tanvihedau28@gmail.com>
2026-01-06 00:12:48 +0530

    u

---
+++ script.js
@@ -0,0 +1,12 @@
+const filters = {
+    brightness: 100,
+    contrast: 100,
+    exposure: 100,
+    saturation: 100,
+    hueRotation: 0,
+    blur: 0,
+    grayscale: 0,
+    sepia: 0,
+    opacity: 100,
+    invert:0
+}

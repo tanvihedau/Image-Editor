c8c23d9ed2f614191eeaf663792713804f5d7609
tanvihedau <tanvihedau28@gmail.com>
2026-04-23 00:39:47 +0530

    fix: correct typos in button labels and add image upload functionality with canvas support

--- script.js
+++ script.js
@@ -61,6 +61,14 @@ const filters = {
     max: 100,
   },
 }
+const imageCanvas = document.querySelector("#image-canvas")
+
+const imgInput = document.querySelector("#image-input")
+
+const canvasCtx = imageCanvas.getContext("2d")
+let file = null
+let image = null
+
 const filtersContainer = document.querySelector(".filters")
 
 //fuction to create filter element
@@ -82,6 +90,13 @@ function createFilterElement(name, unit="%", value, min, max) {
   div.appendChild(p)
   div.appendChild(input)
 
+  input.addEventListener("input", (event) => {
+    filters[name].value = input.value
+    applyFilters()
+
+    
+  })
+
   return div
 }
 //filter object into array and loop through it to create filter elements
@@ -89,3 +104,23 @@ Object.keys(filters).forEach(key => {
   const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
   filtersContainer.appendChild(filterElement)                      
 })
+
+imgInput.addEventListener("change", (event) => {
+  const file = event.target.files[0] 
+  const imagePlaceHolder = document.querySelector(".placeholder")
+  imagePlaceHolder.style.display = "none" //hide the placeholder when an image is selected
+
+  const img = new Image() //create new image element
+  img.src  = URL.createObjectURL(file) //set image source to the selected file  
+  img.onload = () => {
+    image = img
+    imageCanvas.width = img.width //set canvas width to image width
+    imageCanvas.height = img.height //set canvas height to image height
+    canvasCtx.drawImage(img, 0, 0, ) //draw the image on the canvas
+  }
+})
+
+function applyFilters() {
+  canvasCtx.filter = `brightness(${filters.brightness.value}${filters.brightness.unit})`
+  canvasCtx.drawImage(image, 0, 0)
+}
